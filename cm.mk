6f90ac06c6442051375810c2a798e138f6958462
FUKAUMI Naoki <naobsd@gmail.com>
2012-03-25 12:47:08 +0900

    use vertical-320x480 CM9 boot animation
    keep /system small and good for 800x480 screen.

--- cm.mk
+++ cm.mk
@@ -1,6 +1,9 @@
 # Release name
 PRODUCT_RELEASE_NAME := AML8726M
 
+# Boot animation
+TARGET_BOOTANIMATION_NAME := vertical-320x480
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
